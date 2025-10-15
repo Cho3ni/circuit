c65cb709922fba48f81846bed7cce1c33b6d3b6a
Cho3ni <135648772+Cho3ni@users.noreply.github.com>
2025-10-15 12:19:35 -0400

    Update and rename circut.py to majority_test.py

---
+++ majority_test.py
@@ -0,0 +1,24 @@
+
+
+def majority3(in1, in2, in3):
+    """
+    Returns True if at least two of the three inputs are True (or 1).
+    """
+    return (in1 and in2) or (in1 and in3) or (in2 and in3)
+
+def test_majority3():
+    print("All False (0,0,0):", majority3(0, 0, 0))   
+    print("All True  (1,1,1):", majority3(1, 1, 1))  
+
+   
+    print("Inputs (1,1,0):", majority3(1, 1, 0))      
+    print("Inputs (1,0,1):", majority3(1, 0, 1))    
+    print("Inputs (0,1,1):", majority3(0, 1, 1))      
+
+
+    print("Inputs (1,0,0):", majority3(1, 0, 0))     
+    print("Inputs (0,1,0):", majority3(0, 1, 0))    
+    print("Inputs (0,0,1):", majority3(0, 0, 1))     
+
+
+test_majority3()
