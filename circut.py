24a4055c9d6820c8e5faeda0e9f7e05327766985
Cho3ni <135648772+Cho3ni@users.noreply.github.com>
2025-10-15 12:14:35 -0400

    Update circut.py

--- circut.py
+++ circut.py
@@ -1,10 +1,5 @@
-
-def majority(in1, in2, in3):
+def majority3(in1: bool, in2: bool, in3: bool) -> bool:
+    """
+    Returns True if at least two of (in1, in2, in3) are True.
+    """
     return (in1 and in2) or (in1 and in3) or (in2 and in3)
-
-
-if __name__ == "__main__":
-    print(majority(True, True, False))   # True
-    print(majority(True, False, False))  # False
-    print(majority(True, True, True))    # True
-    print(majority(False, False, True))  # False
