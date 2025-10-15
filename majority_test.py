623a841cce24f5a2553b05e94738e0e123e482c8
Cho3ni <135648772+Cho3ni@users.noreply.github.com>
2025-10-15 12:25:47 -0400

    Update majority_test.py

--- majority_test.py
+++ majority_test.py
@@ -1,32 +1,33 @@
 
 """
-Tenzin Choeni
-tenzin.choeni60@myhunter.cuny.edu
-October 15
-makes a true or false
+Name:  Tenzin Choeni
+Email: tenzin.choeni60@myhunter.cuny.edu
+Date:  September 2025
+Takes elevation data of NYC and displays using the default color map
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+elevations = np.loadtxt("elevationsNYC.txt")
 
-def majority3(in1, in2, in3):
-    """
-    Returns True if at least two of the three inputs are True (or 1).
-    """
-    return (in1 and in2) or (in1 and in3) or (in2 and in3)
+intensity = float(input("What intensity for the topo lines: "))
+output_filename = input("What is the output file: ")
 
-def test_majority3():
-    print("All False (0,0,0):", majority3(0, 0, 0))   
-    print("All True  (1,1,1):", majority3(1, 1, 1))  
+mapShape = elevations.shape + (3,)
+topoMap = np.zeros(mapShape)
 
-   
-    print("Inputs (1,1,0):", majority3(1, 1, 0))      
-    print("Inputs (1,0,1):", majority3(1, 0, 1))    
-    print("Inputs (0,1,1):", majority3(0, 1, 1))      
+for row in range(elevations.shape[0]):
+    for col in range(elevations.shape[1]):
+        elev = elevations[row, col]
+        if elev <= 0:
+            topoMap[row, col] = [0.0, 0.0, 1.0]  # Blue
+        elif elev % 10 == 0:
+            topoMap[row, col] = [intensity, intensity, intensity]  # Gray topo line
+        else:
+            topoMap[row, col] = [1.0, 1.0, 1.0]  # White
 
+plt.imsave(output_filename, topoMap)
 
-    print("Inputs (1,0,0):", majority3(1, 0, 0))     
-    print("Inputs (0,1,0):", majority3(0, 1, 0))    
-    print("Inputs (0,0,1):", majority3(0, 0, 1))     
-
-
-test_majority3()
+print("Thank you for using my program!")
+print(f"Your map is stored {output_filename}.")
